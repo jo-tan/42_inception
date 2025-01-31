3953623f1295b55e89b83dd440d8b725b5a8998e
CC <tanjo@umail.iu.edu>
2025-01-31 11:10:00 +0100

    another try

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -9,6 +9,7 @@ echo "Initializing MariaDB data directory..."
 mysql_install_db --user=mysql --datadir=/var/lib/mysql --skip-test-db
 
 echo "Starting temporary MariaDB server..."
+# Start without password protection for initial setup
 mysqld --user=mysql --datadir=/var/lib/mysql &
 
 # Wait for MariaDB to be ready
@@ -18,12 +19,13 @@ while ! mysqladmin ping -h'localhost' --silent; do
 done
 
 echo "Creating database and users..."
-# Create database and users
+# Use mysql without password for initial setup
 mysql -u root << EOF
+-- Set root password first
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};
 CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 
 -- Verify creation
@@ -32,6 +34,7 @@ SELECT User, Host FROM mysql.user;
 EOF
 
 echo "Shutting down temporary MariaDB server..."
+# Use mysqladmin without password since we're still in initial setup
 mysqladmin -u root shutdown
 
 echo "Starting MariaDB server..."
