945a67fc71407ccefb6861dbdd23fe7b774edbe0
CC <tanjo@umail.iu.edu>
2025-01-31 09:58:10 +0100

    edit

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -17,7 +17,9 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql -u root << EOF
     CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};
     CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+    CREATE USER IF NOT EXISTS '${SQL_USER}'@'wordpress.inception' IDENTIFIED BY '${SQL_PASSWORD}';
     GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';
+    GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'wordpress.inception';
     ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
     FLUSH PRIVILEGES;
 EOF
