d9f87a2d3733d93a29f027e8f7114f6bde9d420a
CC <tanjo@umail.iu.edu>
2025-01-31 10:07:21 +0100

    edit

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -13,7 +13,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
         sleep 1
     done
 
-    # Create database and users
+    # Create database and users with broader host permissions
     mysql -u root << EOF
     CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};
     CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
